cbd88f1177e97dac6f2ee3302f18d5f3eb560262
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-12-14 08:25:29 +0530

    Initial commit

---
+++ frontend/src/AppLayout.jsx
@@ -0,0 +1,7 @@
+export function AppLayout() {
+  return (
+    <>
+      <h1>Hi</h1>
+    </>
+  );
+}
---
+++ frontend/src/routes.jsx
@@ -0,0 +1,8 @@
+import { AppLayout } from "./AppLayout";
+
+export const routes = [
+  {
+    path: "/",
+    Component: AppLayout,
+  },
+];
