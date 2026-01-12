11d3f333eb422d1d5d15938fec16e921f0078f2c
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2026-01-12 10:32:36 +0530

    feat: make game work

--- api/app.js
+++ api/app.js
@@ -17,8 +17,18 @@ app.get("/api/game", (req, res) => {
   });
 });
 
+app.post("/api/game/start", async (req, res) => {
+  const session = await prisma.gameSession.create({
+    data: {},
+  });
+
+  const { id } = session;
+
+  res.json({ message: "Game Started", data: id });
+});
+
 app.post("/api/game/play", async (req, res) => {
-  const { character, clickX, clickY } = req.body;
+  const { character, clickX, clickY, id } = req.body;
 
   const characterData = await prisma.character.findUnique({
     where: {
@@ -28,11 +38,31 @@ app.post("/api/game/play", async (req, res) => {
 
   const isCharacterFound = isWithinTolerance(clickX, clickY, characterData);
 
-  if (isCharacterFound) {
-    return res.status(200).json({ message: `${character} Found` });
+  if (!isCharacterFound) {
+    return res.status(200).json({ message: `${character} NOT Found!` });
   }
 
-  return res.status(200).json({ message: `${character} NOT Found!` });
+  const updatedSession = await prisma.gameSession.update({
+    where: { id },
+    data: {
+      foundCharacters: {
+        push: character,
+      },
+    },
+  });
+
+  if (updatedSession.foundCharacters.length === 3) {
+    await prisma.gameSession.update({
+      where: { id },
+      data: {
+        endTime: new Date(),
+      },
+    });
+
+    return res.json({ message: "You Found all characters", gameover: true });
+  }
+
+  return res.status(200).json({ message: `${character} Found` });
 });
 
 module.exports = app;
